b448818e0ce988a6dcd005e09599205734759162
rubinyehuda8@gmail.com <rubinyehuda8@gmail.com>
2025-03-20 13:02:43 +0200

    Added Point and Vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -2,6 +2,17 @@ package primitives;
 
 public class Point {
     protected Double3 xyz;
+    public static Point ZERO = (0,0,0);
+
+    public Point(double x, double y, double z) {
+        xyz = new Double3(x, y, z);
+    }
+
+    public Point(Double3 xyz) {
+        this.xyz = xyz;
+    }
+
+
 }
 
 
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,4 +1,22 @@
 package primitives;
 
-public class Vector {
+public class Vector extends Point {
+
+    public  Vector(double x, double y, double z) {
+        super(x, y, z);
+        if(x==0 && y==0 && z==0) {
+            throw new IllegalArgumentException("Vector cannot be (0,0,0)");
+        }
+    }
+
+    public Vector(Double3 xyz) {
+        super(xyz);
+        if(xyz.d1==0 && xyz.d2==0 && xyz.d3==0) {
+            throw new IllegalArgumentException("Vector cannot be (0,0,0)");
+        }
+    }
+
+    public add(Vector v) {
+        return new Vector(xyz.d1 + v.xyz.d1, xyz.d2 + v.xyz.d2, xyz.d3 + v.xyz.d3);
+    }
 }
