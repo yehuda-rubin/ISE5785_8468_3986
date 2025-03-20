c82d72c41dfe197b576ca25c34f7bb6c962b2d32
rubinyehuda8@gmail.com <rubinyehuda8@gmail.com>
2025-03-20 16:06:48 +0200

    Added Point and Vector

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,4 +1,11 @@
 package primitives;
 
 public class Ray {
+    private final Point head;
+    private final Vector direction;
+
+    public Ray(Point head, Vector direction) {
+        this.head = head;
+        this.direction = direction.normalize();
+    }
 }
