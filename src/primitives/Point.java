73a1ccda941bc3584afbda68f3ecf699739b7b61
rubinyehuda8@gmail.com <rubinyehuda8@gmail.com>
2025-03-20 15:16:08 +0200

    Added Point and Vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,31 +1,66 @@
 package primitives;
 
+/**
+ * This class make point in 3D space
+ *  xyz is a point
+ * @author    Yehuda rubin and arye hacohen
+ */
 public class Point {
     protected Double3 xyz;
-    public static Point ZERO = (0,0,0);
+    public static Point ZERO = new Point(0, 0, 0);
 
+    /**
+     * constructor
+     * @param x will be d1 value
+     * @param y will be d2 value
+     * @param z will be d3 value
+     */
     public Point(double x, double y, double z) {
         xyz = new Double3(x, y, z);
     }
 
+    /**
+     * constructor
+     * @param xyz will be the point
+     */
     public Point(Double3 xyz) {
         this.xyz = xyz;
     }
 
+    /**
+     * make subtraction between two points
+     * @param p is the point that we subtract from the current point
+     * @return the result of the subtraction
+     */
     public Point subtract(Point p) {
-        return new Point(xyz.d1 - p.xyz.d1, xyz.d2 - p.xyz.d2, xyz.d3 - p.xyz.d3);
+        return new Point(xyz.d1() - p.xyz.d1(), xyz.d2() - p.xyz.d2(), xyz.d3() - p.xyz.d3());
     }
 
+    /**
+     * make addition between two points
+     * @param v is the vector that we add to the current point
+     * @return the result of the addition
+     */
     public Point add(Vector v) {
-        return new Point(xyz.d1 + v.xyz.d1, xyz.d2 + v.xyz.d2, xyz.d3 + v.xyz.d3);
+        return new Point(xyz.d1() + v.xyz.d1(), xyz.d2() + v.xyz.d2(), xyz.d3() + v.xyz.d3());
     }
 
+    /**
+     * make distanceSquared between two points
+     * @param p is the point that we distanceSquared from the current point
+     * @return the result of the distanceSquared
+     */
     public Point distanceSquared(Point p) {
-        return new Point((xyz.d1 - p.xyz.d1) * (xyz.d1 - p.xyz.d1) +
-                (xyz.d2 - p.xyz.d2) * (xyz.d2 - p.xyz.d2) +
-                (xyz.d3 - p.xyz.d3) * (xyz.d3 - p.xyz.d3));
+        return new Point((xyz.d1() - p.xyz.d1()) * (xyz.d1() - p.xyz.d1()) +
+                (xyz.d2() - p.xyz.d2()) * (xyz.d2() - p.xyz.d2()) +
+                (xyz.d3() - p.xyz.d3()) * (xyz.d3() - p.xyz.d3()));
     }
 
+    /**
+     * make distance between two points
+     * @param p is the point that we distance from the current point
+     * @return the result of the distance
+     */
     public Point distance(Point p) {
         return new Point(Math.sqrt(distanceSquared(p)));
     }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,7 +1,18 @@
 package primitives;
 
+/**
+ * This class make vector in 3D space
+ *  xyz is a vector
+ * @author    Yehuda rubin and arye hacohen
+ */
 public class Vector extends Point {
 
+    /**
+     * constructor
+     * @param x will be d1 value
+     * @param y will be d2 value
+     * @param z will be d3 value
+     */
     public  Vector(double x, double y, double z) {
         super(x, y, z);
         if(x==0 && y==0 && z==0) {
@@ -9,33 +20,42 @@ public class Vector extends Point {
         }
     }
 
+    /**
+     * constructor
+     * @param xyz will be the vector
+     */
     public Vector(Double3 xyz) {
         super(xyz);
-        if(xyz.d1==0 && xyz.d2==0 && xyz.d3==0) {
+        if(xyz.d1()==0 && xyz.d2()==0 && xyz.d3()==0) {
             throw new IllegalArgumentException("Vector cannot be (0,0,0)");
         }
     }
 
+    /**
+     * make add between two vectors
+     * @param v is the vector that we add from the current vector
+     * @return the result of the add
+     */
     public Vector add(Vector v) {
-        return new Vector(xyz.d1 + v.xyz.d1, xyz.d2 + v.xyz.d2, xyz.d3 + v.xyz.d3);
+        return new Vector(xyz.d1() + v.xyz.d1(), xyz.d2() + v.xyz.d2(), xyz.d3() + v.xyz.d3());
     }
 
     public Vector scale(double scalar) {
-        return new Vector(xyz.d1 * scalar, xyz.d2 * scalar, xyz.d3 * scalar);
+        return new Vector(xyz.d1() * scalar, xyz.d2() * scalar, xyz.d3() * scalar);
     }
 
     public Vector dotProduct(Vector v) {
-        return new Vector(xyz.d1 * v.xyz.d1, xyz.d2 * v.xyz.d2, xyz.d3 * v.xyz.d3);
+        return new Vector(xyz.d1() * v.xyz.d1(), xyz.d2() * v.xyz.d2(), xyz.d3() * v.xyz.d3());
     }
 
     public Vector crossProduct(Vector v) {
-        return new Vector(xyz.d2 * v.xyz.d3 - xyz.d3 * v.xyz.d2,
-                xyz.d3 * v.xyz.d1 - xyz.d1 * v.xyz.d3,
-                xyz.d1 * v.xyz.d2 - xyz.d2 * v.xyz.d1);
+        return new Vector(xyz.d2() * v.xyz.d3() - xyz.d3() * v.xyz.d2(),
+                xyz.d3() * v.xyz.d1() - xyz.d1() * v.xyz.d3(),
+                xyz.d1() * v.xyz.d2() - xyz.d2() * v.xyz.d1());
     }
 
     public double lengthSquared() {
-        return (xyz.d1 * xyz.d1 + xyz.d2 * xyz.d2 + xyz.d3 * xyz.d3);
+        return (xyz.d1() * xyz.d1() + xyz.d2() * xyz.d2() + xyz.d3() * xyz.d3());
     }
 
     public double length() {
