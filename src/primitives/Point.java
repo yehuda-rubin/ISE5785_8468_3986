0faedcb3f25b54335047730d6691910b8b4ac5d2
rubinyehuda8@gmail.com <rubinyehuda8@gmail.com>
2025-03-20 18:45:27 +0200

    Added Point and Vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -38,10 +38,10 @@ public class Point {
      *
      * @param p is the point that we subtract from the current point
      * @return the result of the subtraction
-     */
+     */S
 
-    public Point subtract(Point p) {
-        return new Point(xyz.d1() - p.xyz.d1(), xyz.d2() - p.xyz.d2(), xyz.d3() - p.xyz.d3());
+    public Vector subtract(Point p) {
+        return new Vector(xyz.d1() - p.xyz.d1(), xyz.d2() - p.xyz.d2(), xyz.d3() - p.xyz.d3());
     }
 
     /**
