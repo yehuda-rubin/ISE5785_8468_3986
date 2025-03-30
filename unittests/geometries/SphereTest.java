4af14ff7bbd02e1bc3e6a53355cb2f1f5aaa9226
rubinyehuda8@gmail.com <rubinyehuda8@gmail.com>
2025-03-30 18:25:24 +0300

    test

--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -38,5 +38,32 @@ class SphereTest {
         Sphere s = new Sphere(1, new Point(0, 0, 0));
         assertEquals(new Vector(1, 0, 0), s.getNormal(new Point(1, 0, 0)), "Normal to a sphere does not work correctly");
 
+        // ============ Boundary Values Tests ==================
+        // TC02: Normal to a sphere
+        // The normal to the sphere at the point (0, 1, 0)
+        assertEquals(new Vector(0, 1, 0), s.getNormal(new Point(0, 1, 0)), "Normal to a sphere does not work correctly");
+
+        // TC03: Normal to a sphere
+        // The normal to the sphere at the point (0, 0, 1)
+        assertEquals(new Vector(0, 0, 1), s.getNormal(new Point(0, 0, 1)), "Normal to a sphere does not work correctly");
+
+        // TC04: Normal to a sphere
+        // The normal to the sphere at the point (0, -1, 0)
+        assertEquals(new Vector(0, -1, 0), s.getNormal(new Point(0, -1, 0)), "Normal to a sphere does not work correctly");
+
+        // TC05: Normal to a sphere
+        // The normal to the sphere at the point (0, 0, -1)
+        assertEquals(new Vector(0, 0, -1), s.getNormal(new Point(0, 0, -1)), "Normal to a sphere does not work correctly");
+
+        // TC06: Normal to a sphere
+        // The normal to the sphere at the point (-1, 0, 0)
+        assertEquals(new Vector(-1, 0, 0), s.getNormal(new Point(-1, 0, 0)), "Normal to a sphere does not work correctly");
+
+        // TC07: Normal to a sphere
+        // The normal to the sphere at the point (0, 0, 0)
+        // The normal to the sphere at the point (0, 0, 0) is not defined
+        // because the point is the center of the sphere
+        assertThrows(IllegalArgumentException.class, () -> s.getNormal(new Point(0, 0, 0)), "Failed to throw an exception when getting the normal to a sphere at the center");
+
     }
 }
