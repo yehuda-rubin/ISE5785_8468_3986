e2601e9bcc543ce85c635330b112624c418289d0
rubinyehuda8@gmail.com <rubinyehuda8@gmail.com>
2025-05-06 15:29:27 +0300

    test

---
+++ unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,29 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTest {
+    /**
+     * A table of 16 by 10 squares, each square measuring 50 by 50 pixels, with the inside of the square colored yellow and the outside red.
+     */
+    @Test
+    void testWriteToImage() {
+        assertDoesNotThrow(() -> {
+            Color yellow = new Color(java.awt.Color.YELLOW);
+            Color red = new Color(java.awt.Color.RED);
+            ImageWriter imageWriter = new ImageWriter(801, 501);
+            for (int i = 0; i < 801; i++) {
+                for (int j = 0; j < 501; j++) {
+                    if (i % 50 == 0 || j % 50 == 0) {
+                        imageWriter.writePixel(i, j, yellow);
+                    } else {
+                        imageWriter.writePixel(i, j, red);
+                    }
+                }
+            }
+        }, "Failed to create image");
+    }
+}
